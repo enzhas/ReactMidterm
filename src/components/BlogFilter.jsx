1c12d3bf55da204e284c3165add693101eb34ba0
enzhas <nurgan.kurmash@mail.ru>
2023-10-28 15:57:17 +0600

    ll

--- src/components/BlogFilter.jsx
+++ src/components/BlogFilter.jsx
@@ -1,29 +1,36 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-const BlogFilter = ({postQuery, latest, setSearchParams}) => {
-    const [search, setSearch] = useState(postQuery);
-    const [checked, setChecked] = useState(latest);
+const BlogFilter = ({ postQuery, latest, setSearchParams }) => {
+  const [search, setSearch] = useState(postQuery);
+  const [checked, setChecked] = useState(latest);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const query = form.search.value;
-        const isLatest = form.latest.checked;
-        const params = {};
-        if (query.length) params.post = query;
-        if (isLatest) params.latest = true;
-        setSearchParams(params);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const query = form.search.value;
+    const isLatest = form.latest.checked;
+    const params = {};
+
+    if (query.length) {
+      params.post = query;
     }
 
-    return (
-        <form autoComplete="off" onSubmit={handleSubmit}>
-            <input type="search" name="search" value={search} onChange={e => setSearch(e.target.value)} />
-            <label style={{ padding: '0 1rem' }}>
-                <input type="checkbox" name="latest" checked={checked} onChange={e => setChecked(e.target.checked)}/> New only
-            </label>
-            <input type="submit" value="Search" />
-        </form>
-    );
+    if (isLatest) {
+      params.latest = true;
+    }
+
+    setSearchParams(params);
+  };
+
+  return (
+    <form autoComplete="off" onSubmit={handleSubmit}>
+      <input type="search" name="search" value={search} onChange={e => setSearch(e.target.value)} />
+      <label style={{ padding: '0 1rem' }}>
+        <input type="checkbox" name="latest" checked={checked} onChange={e => setChecked(e.target.checked)}/> New only
+      </label>
+      <input type="submit" value="Search" />
+    </form>
+  );
 };
 
 export { BlogFilter };
--- src/pages/Singlepage.jsx
+++ src/pages/Singlepage.jsx
@@ -1,34 +1,38 @@
-import { useParams, Link, useNavigate } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const Singlepage = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const navigate = useNavigate();
     const [post, setPost] = useState(null);
+    const [likeCount, setLikeCount] = useState(0);
 
     const goBack = () => navigate(-1);
-    const goHome = () => navigate('/', {replace: true});
+    const goHome = () => navigate('/', { replace: true });
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then(res => res.json())
-            .then(data => setPost(data))
+            .then(data => setPost(data));
     }, [id]);
+
+    const handleLikeClick = () => setLikeCount(likeCount + 1);
 
     return (
         <div>
             <button onClick={goBack}>Go back</button>
-            {/* Bad approach */}
             <button onClick={goHome}>Go home</button>
             {post && (
                 <>
                     <h1>{post.title}</h1>
                     <p>{post.body}</p>
                     <Link to={`/posts/${id}/edit`}>Edit this post</Link>
+                    <p>{likeCount}</p>
+                    <button onClick={handleLikeClick}>Like</button>
                 </>
             )}
         </div>
-    )
+    );
 }
 
-export {Singlepage}
+export { Singlepage };
